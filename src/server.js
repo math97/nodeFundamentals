6b835cddddcaaa6921630e8436a04c4d7dc5995d
Matheus Albuquerque <math.albuquerque@outlook.com>
2024-02-12 19:26:57 +0000

    Create manual route and use regex for path

--- src/server.js
+++ src/server.js
@@ -1,34 +1,20 @@
 import http from 'http'
-import {randomUUID} from 'node:crypto'
 import { json } from './middlewares/json.js'
-import { Database } from './database.js'
+import { routes } from './routes.js'
 
-
-const database = new Database()
 
 const server = http.createServer(async (req,res)=>{
     const {method,url}= req
 
     await json(req,res)
 
-    if(method === 'GET' && url === '/users'){
-        const users = database.select('users')
-        return res.setHeader('Content-type','application/json').end(JSON.stringify(users))
-    }
+    const route = routes.find(route =>{
+        return route.method === method && route.path.test(url)
+    })
 
-
-    if(method === 'POST' && url === '/users'){
-        const {name,email}= req.body
-
-        const user ={
-            id:randomUUID(),
-            name,
-            email
-        }
-
-        database.insert('users',user)
-        return res.writeHead(201).end()
-    }
+    if(route) {
+        const routeParams = req.url.match(route.path)
+        return route.handler(req,res)}
 
     return res.writeHead(404).end('Not found')
 })
